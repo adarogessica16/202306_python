74742dea52f1963cf9d6f5e32a56bae881005124
Gessica <adarogessica@gmail.com>
2023-07-13 15:14:54 -0400

    ejercicios de practica

---
+++ Practica/Ejercicio-holaMundo/hola_mundo.py
@@ -0,0 +1,21 @@
+# 1. Imprimir "Hola, mundo"
+print("Hola, mundo")
+
+# 2. Imprimir "Hola, Noelle" con el nombre en una variable
+name = " Gessica"
+print( "Hola, " , name )	# con una coma
+print("Hola, " + name)  # con un +
+
+# 3. Imprimir "Hola 42!" con el número en una variable
+number = 42
+print( f"Hola, {number}!" )	# con una coma
+print("Hola " + str(number) + "!")  # con un +
+
+# 4. Imprimir "Amo comer sushi y pizza" con las comidas en variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("Amo comer {} y {}".format(fave_food1, fave_food2))  # con .format()
+
+print(f"Amo comer {fave_food1} y {fave_food2}")  # con una cadena f
+
+
