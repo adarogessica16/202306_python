db194a8a86f1ae2d6f99c825734f3010b0cf2197
Gessica <adarogessica@gmail.com>
2023-07-23 13:27:38 -0400

    Practica de POO

---
+++ OOP/Practica/claseUsuario.py
@@ -0,0 +1,37 @@
+class Usuario:	
+    def __init__(self, nombre, email, balance_cuenta=0):
+        self.nombre = nombre
+        self.email = email
+        self.balance_cuenta = balance_cuenta
+
+    # agregando el método de depósito
+    def hacer_depósito(self, monto):	
+        self.balance_cuenta += monto	# la cuenta del usuario específico aumenta en la cantidad del valor recibido
+
+    def hacer_retiro(self, monto):
+        self.balance_cuenta -= monto
+
+    def mostrar_balance_usuario(self):
+        print(f"Balance de {self.nombre}: ${self.balance_cuenta:.2f}")
+    
+    def transferir(self, usuarioDestino, monto):
+        if monto<= self.balance_cuenta:
+            self.balance_cuenta-= monto
+            usuarioDestino.hacer_depósito(monto)
+            print(f"Transferencia exitosa. {self.nombre} ha transferido ${monto:.2f} a {usuarioDestino.nombre}.")
+        else:
+            print("No se pudo hacer la transferencia")
+
+Usuario1= Usuario("Gessica", "adarogessia")
+Usuario2= Usuario("Jose", "adarogia")
+Usuario3= Usuario("Jazz", "@@@")
+Usuario1.hacer_depósito(500)
+Usuario1.hacer_depósito(100)
+Usuario1.hacer_depósito(50)
+Usuario1.mostrar_balance_usuario()
+Usuario1.transferir(Usuario2, 400)
+Usuario1.mostrar_balance_usuario()
+Usuario2.mostrar_balance_usuario()
+Usuario3.hacer_depósito(1000)
+Usuario3.hacer_retiro(500)
+Usuario3.mostrar_balance_usuario()
